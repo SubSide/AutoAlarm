d0a1c6a7e7b5186cf9c462c7ff0edab1fd04a9e9
Thomas 'SubSide' van den Bulk <Thomasvdbulk@gmail.com>
2017-11-17 18:11:39 +0100

    Added: Selectable calendars

--- app/src/main/java/nl/thomasvdbulk/autoalarm/MainActivity.java
+++ app/src/main/java/nl/thomasvdbulk/autoalarm/MainActivity.java
@@ -98,13 +98,6 @@ public class MainActivity extends BaseActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
--- app/src/main/java/nl/thomasvdbulk/autoalarm/PickCalendarActivity.java
+++ app/src/main/java/nl/thomasvdbulk/autoalarm/PickCalendarActivity.java
@@ -15,8 +15,11 @@ import android.provider.BaseColumns;
 import android.provider.CalendarContract;
 import android.app.LoaderManager;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.ActionMode;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -30,8 +33,7 @@ import android.widget.TextView;
 import java.util.HashSet;
 import java.util.Set;
 
-public class PickCalendarActivity extends ListActivity
-        implements LoaderManager.LoaderCallbacks<Cursor> {
+public class PickCalendarActivity extends BaseActivity {
 
 
     // This is the Adapter being used to display the list's data
@@ -44,84 +46,86 @@ public class PickCalendarActivity extends ListActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pick_calendar);
 
-        // For the cursor adapter, specify which columns go into which views
-        String[] fromColumns = { CalendarContract.Calendars.NAME, BaseColumns._ID };
-        int[] toViews = { R.id.item }; // The TextView in simple_list_item_1
+        // Get and init Recyclerview
+        LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LinearLayout linearLayout = findViewById(R.id.calendar_list);
 
-        // Create an empty adapter we will use to display the loaded data.
-        // We pass null for the cursor, then update it in onLoadFinished()
-        mAdapter = new SimpleCursorAdapter(this,
-                R.layout.calendar_list_item, null,
-                fromColumns, toViews, 0);
-        setListAdapter(mAdapter);
+        //Get the preferences
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        itemIds = sharedPref.getStringSet(MainActivity.DATA_CALENDAR_ID_KEY, new HashSet<String>());
 
-        mAdapter.setViewBinder(new SimpleCursorAdapter.ViewBinder(){
+        // Load the calendars
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED){
+            return;
+        }
 
+
+        String[] projection = new String[] { BaseColumns._ID, CalendarContract.Calendars.NAME };
+        Cursor cur = getContentResolver().query(CalendarContract.Calendars.CONTENT_URI, projection, null, null, null);
+
+        View.OnClickListener listener = new View.OnClickListener(){
             @Override
-            public boolean setViewValue(View view, Cursor cursor, int i) {
-                if(view instanceof TextView) {
-                    view.setTag(R.string.calendar_id, cursor.getInt(cursor.getColumnIndex(BaseColumns._ID)));
-                    view.setTag(R.string.calendar_selected, false);
-                    // TODO set selected to true and show image if already selected
+            public void onClick(View v) {
+                TextView view = v.findViewById(R.id.item);
+                boolean isSelected = view.getTag(R.string.calendar_selected) != null && (boolean)view.getTag(R.string.calendar_selected);
+                // If the tag is null or false, we set it to true and the other way around.
+                isSelected = !isSelected;
+                view.setTag(R.string.calendar_selected, isSelected);
+
+                if(isSelected){
+                    itemIds.add(""+view.getTag(R.string.calendar_id));
+                } else {
+                    itemIds.remove(""+view.getTag(R.string.calendar_id));
                 }
-                return false;
+
+                // We grab the image so we can check if it is selected or not
+                ImageView img = v.findViewById(R.id.checkbox);
+
+                img.setVisibility(isSelected ? View.VISIBLE : View.GONE);
+            }
+        };
+
+        while(cur.moveToNext()) {
+            // Create a new view
+            View calendar = inflater.inflate(R.layout.calendar_list_item, null);
+            // Set click handler
+            calendar.setOnClickListener(listener);
+
+            // Set the ID tag
+            calendar.setTag(R.string.calendar_id, cur.getInt(cur.getColumnIndex(BaseColumns._ID)));
+            // Set the text to the calendar name
+            TextView text = calendar.findViewById(R.id.item);
+            text.setText(cur.getString(cur.getColumnIndex(CalendarContract.Calendars.NAME)));
+
+            // If the view was already selected in the past, we want to set the tag to true and show the checkmark
+            if(itemIds.contains(""+cur.getInt(cur.getColumnIndex(BaseColumns._ID)))){
+                calendar.setTag(R.string.calendar_selected, true);
+                calendar.findViewById(R.id.checkbox).setVisibility(View.VISIBLE);
+            } else {
+                calendar.setTag(R.string.calendar_selected, false);
+            }
+            linearLayout.addView(calendar);
+
+            Log.d("Calendar info", cur.getString(cur.getColumnIndex(CalendarContract.Calendars.NAME)));
+        }
+        cur.close();
+
+        // Now add a button onclick handler
+        findViewById(R.id.button_done).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
             }
         });
-
-        // Prepare the loader.  Either re-connect with an existing one,
-        // or start a new one.
-        getLoaderManager().initLoader(0, null, this);
-    }
-
-    // Called when a new Loader needs to be created
-    @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        // Now create and return a CursorLoader that will take care of
-        // creating a Cursor for the data being displayed.
-        return new CursorLoader(this, CalendarContract.Calendars.CONTENT_URI,
-                new String[]{ BaseColumns._ID, CalendarContract.Calendars.NAME }, null, null, null);
-    }
-
-    // Called when a previously created loader has finished loading
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        // Swap the new cursor in.  (The framework will take care of closing the
-        // old cursor once we return.)
-        mAdapter.swapCursor(data);
-    }
-
-    // Called when a previously created loader is reset, making the data unavailable
-    public void onLoaderReset(Loader<Cursor> loader) {
-        // This is called when the last Cursor provided to onLoadFinished()
-        // above is about to be closed.  We need to make sure we are no
-        // longer using it.
-        mAdapter.swapCursor(null);
-    }
-
-
-
-    @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        TextView view = v.findViewById(R.id.item);
-        boolean isSelected = view.getTag(R.string.calendar_selected) != null && (boolean)view.getTag(R.string.calendar_selected);
-        // If the tag is null or false, we set it to true and the other way around.
-        isSelected = !isSelected;
-        view.setTag(R.string.calendar_selected, isSelected);
-
-        // We grab the image so we can check if it is selected or not
-        ImageView img = v.findViewById(R.id.checkbox);
-
-        img.setVisibility(isSelected ? View.VISIBLE : View.GONE);
     }
 
     @Override
     public void onBackPressed() {
-//        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
-//        SharedPreferences.Editor editor = sharedPref.edit();
-//
-//
-//
-//        editor.putStringSet(MainActivity.DATA_CALENDAR_ID_KEY, newHighScore);
-//        editor.commit();
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        editor.putStringSet(MainActivity.DATA_CALENDAR_ID_KEY, itemIds);
+        editor.apply();
 
         super.onBackPressed();
     }
